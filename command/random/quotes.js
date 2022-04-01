b0fffb71cea5acced62e822bfd9b5d7b6ac18d47
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-01 19:50:29 +0700

    Update quotes.js

--- command/random/quotes.js
+++ command/random/quotes.js
@@ -2,9 +2,8 @@ module.exports = {
 	name: "quotesanime",
 	alias: ["quotes", "animequotes", "quote", "quoteanime"],
 	category: "random",
-        options: {
+        isSpam: true,
 	wait: true,
-        }
 	async run(msg, conn, q, isOwner, body, map, config, args) {
 		var animquote = await rzky.random.quotesAnime();
 		var animrandom = animquote.result[Math.floor(Math.random() * animquote.result.length)];
