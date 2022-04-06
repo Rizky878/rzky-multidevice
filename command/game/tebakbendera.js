3a3255a6c9cb80a6889a33e4cb0df04bdaf8f6e9
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-07 05:02:12 +0700

    Add files via upload

--- command/game/tebakbendera.js
+++ command/game/tebakbendera.js
@@ -1,1 +1,19 @@
+const { cekStatus, addSesi } = require('../../lib/optiongame')
 
+module.exports = {
+	name: "tebakbendera",
+	alias: ["gamebendera"],
+	category: "game",
+	desc: "Bermain game, menebak bendera negara",
+	isSpam: true,
+	isLimitGame: true,
+    async run(msg, conn, q, isOwner, body, map) {
+    if(cekStatus(msg.from, map, "tebakbendera")) throw "Group Ini masih dalam sesi permainan"
+    let waktugame = 60
+    let tebakbendera = await rzky.game.tebakbendera()
+    if(tebakbendera) {
+    addSesi(msg.from, tebakbendera.jawaban, waktugame, map, "tebakbendera")
+    await msg.reply(`*Game Tebak Bendera*\n\nBendera: ${tebakbendera.bendera}\nHint: ${tebakbendera.pertanyaan}\n\nSegera Dijawab, Waktu hanya 1 menit!`)
+    } else msg.reply( "Error" )
+  }
+}
