aa5493841642dc70ef223007d0a7bfe00cdaf035
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-13 00:48:22 +0700

    Update eval.js

--- command/owner/eval.js
+++ command/owner/eval.js
@@ -2,16 +2,16 @@ module.exports = {
 	name: "eval",
 	alias: [">>", ">"],
 	category: "private",
-        noPrefix: true,
+	noPrefix: true,
         isOwner: true,
 	desc: "running javascript code via command can also test something code",
 	use: `">" <code javascript> with await and ">>" <code> live return or immediately show the result`,
+	query: `Masukan Parameter Code`,
 	async run(msg, conn, q, map, args, arg) {
-                let { body } = msg
-		let kode = body.trim().split(/ +/);
+		let kode = msg.body.trim().split(/ +/)[0]
 		let teks;
 		try {
-			teks = await eval(`(async () => { ${q}})()`);
+			teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`);
 		} catch (e) {
 			teks = e;
 		} finally {
