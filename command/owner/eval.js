af020110b59415ca8e4c89303db4f1b018acf343
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-12 07:30:09 +0700

    Update eval.js

--- command/owner/eval.js
+++ command/owner/eval.js
@@ -7,6 +7,7 @@ module.exports = {
 	desc: "running javascript code via command can also test something code",
 	use: `">" <code javascript> with await and ">>" <code> live return or immediately show the result`,
 	async run(msg, conn, q, map, args, arg) {
+                let { body } = msg
 		let kode = body.trim().split(/ +/);
 		let teks;
 		try {
