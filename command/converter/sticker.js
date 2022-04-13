9844c51b83dca929f99582a9b065278aaae3db98
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-13 17:33:05 +0700

    Update sticker.js

--- command/converter/sticker.js
+++ command/converter/sticker.js
@@ -2,10 +2,9 @@ const { sticker } = require("../../lib/convert");
 
 module.exports = {
 	name: "sticker",
-	alias: ["s","stick", "stik", "stiker", "stickergif", "stikergif", "gifstiker", "gifsticker"],
+	alias: ["stick", "stik", "stiker", "take", "swm", "stickerwm", "wm", "stickergif", "stikergif", "gifstiker", "gifsticker"],
 	category: "converter",
 	desc: "Create a sticker from image or video",
-        isSpam: true,
 	async run(msg, conn) {
 		const { quoted, from, type } = msg;
 
@@ -14,34 +13,32 @@ module.exports = {
 		const isQImg = type === "extendedTextMessage" && content.includes("imageMessage");
 		const isQVid = type === "extendedTextMessage" && content.includes("videoMessage");
 		const isQDoc = type === "extendedTextMessage" && content.includes("documentMessage");
+		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage");
+		q = q.split("|")
+		const packInfo = { "packname" : q[0] ? q[0] : config.packInfo.packname, "author": q[1] ? q[1] : config.packInfo.author }
 
 		let buffer, stickerBuff;
 		try {
-			if ((isMedia && !msg.message.videoMessage) || isQImg) {
-				buffer = isQImg ? await quoted.download() : await msg.download();
-				stickerBuff = await sticker(buffer, { isImage: true, cmdType: "1" });
+		if (isQStic) {
+				buffer = await quoted.download()
+				stickerBuff = await sticker(buffer, { isSticker: true, withPackInfo: true, packInfo, cmdType: "1" });
 				await conn.sendMessage(from, { sticker: stickerBuff }, { quoted: msg });
-			} else if (
-				(isMedia && msg.message.videoMessage.fileLength < 2 << 20) ||
-				(isQVid && quoted.message.videoMessage.fileLength < 2 << 20)
-			) {
+			} else if ((isMedia && !msg.message.videoMessage) || isQImg) {
+				buffer = isQImg ? await quoted.download() : await msg.download();
+				stickerBuff = await sticker(buffer, { isImage: true, withPackInfo: true, packInfo, cmdType: "1" });
+				await conn.sendMessage(from, { sticker: stickerBuff }, { quoted: msg });
+			} else if ((isMedia && msg.message.videoMessage.fileLength < 2 << 20) || (isQVid && quoted.message.videoMessage.fileLength < 2 << 20)) {
 				buffer = isQVid ? await quoted.download() : await msg.download();
-				stickerBuff = await sticker(buffer, { isVideo: true, cmdType: "1" });
+				stickerBuff = await sticker(buffer, { isVideo: true, withPackInfo: true, packInfo, cmdType: "1" });
 				await conn.sendMessage(from, { sticker: stickerBuff }, { quoted: msg });
 			} else if (
 				isQDoc &&
-				(/image/.test(quoted.message.documentMessage.mimetype) ||
-					(/video/.test(quoted.message.documentMessage.mimetype) &&
-						quoted.message.documentMessage.fileLength < 2 << 20))
+				(/image/.test(quoted.message.documentMessage.mimetype) || (/video/.test(quoted.message.documentMessage.mimetype) && quoted.message.documentMessage.fileLength < 2 << 20))
 			) {
-				let ext = /image/.test(quoted.message.documentMessage.mimetype)
-					? { isImage: true }
-					: /video/.test(quoted.message.documentMessage.mimetype)
-					? { isVideo: true }
-					: null;
+				let ext = /image/.test(quoted.message.documentMessage.mimetype) ? { isImage: true } : /video/.test(quoted.message.documentMessage.mimetype) ? { isVideo: true } : null;
 				if (!ext) return await msg.reply("Document mimetype unknown");
 				buffer = await quoted.download();
-				stickerBuff = await sticker(buffer, { ...ext, cmdType: "1" });
+				stickerBuff = await sticker(buffer, { ...ext, withPackInfo: true, packInfo, cmdType: "1" });
 				await conn.sendMessage(from, { sticker: stickerBuff }, { quoted: msg });
 			} else {
 				await msg.reply(`reply sticker`);
