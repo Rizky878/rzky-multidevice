3f15255b89468d352b0cc6568816c77f2fc1862b
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-18 05:20:01 +0700

    Create scriptbot.js

---
+++ command/info/scriptbot.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: "scriptbot",
+	alias: ["script","sc","scbot"],
+	category: "info",
+	isSpam: true,
+	async run(msg, conn, q, map, args) {
+	  await conn.sendMessage(msg.from, { image: { url: config.thumb }, footer: config.namebot, caption: `Script Bot Is here\ndon't forget fork + star XD`, templateButtons: [{ urlButton: { displayText: "Script Bot", url: "https://github.com/Rizky878/rzky-multidevice/" }}]}, {quoted: msg})
+	}
+}
