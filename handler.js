ceee39188327e37c7f00c9247e83f855bff0182d
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-17 23:36:38 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -283,7 +283,7 @@ module.exports = handler = async (m, conn, map) => {
 			await msg.reply(typeof options.wait == "string" ? options.wait : response.wait);
 		}
 		try {
-			await cmd.run(msg, conn, q, isOwner, body, map, config, args, arg);
+			await cmd.run(msg, conn, q, map, args, arg);
 		} catch (e) {
 			await msg.reply(require("util").format(e));
 		}
