8d8d63f24615259e402fc45054bf7df28836ae62
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-18 16:09:31 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,15 +1,11 @@
+require("./global.js")
 const { getBinaryNodeChild } = require("@adiwajshing/baileys");
 const { serialize } = require("./lib/serialize");
 const fs = require("fs");
-const{isLimit:isLimit,limitAdd:limitAdd,getLimit:getLimit,giveLimit:giveLimit,addBalance:addBalance,kurangBalance:kurangBalance,getBalance:getBalance,isGame:isGame,gameAdd:gameAdd,givegame:givegame,cekGLimit:cekGLimit}=require("./lib/limit");global.prem=require("./lib/premium"),global.limit=JSON.parse(fs.readFileSync("./database/limit.json")),global.glimit=JSON.parse(fs.readFileSync("./database/glimit.json")),global.balance=JSON.parse(fs.readFileSync("./database/balance.json")),global.premium=JSON.parse(fs.readFileSync("./database/premium.json")),global.isLimit=isLimit,global.limitAdd=limitAdd,global.getLimit=getLimit,global.giveLimit=giveLimit,global.addBalance=addBalance,global.kurangBalance=kurangBalance,global.getBalance=getBalance,global.isGame=isGame,global.gameAdd=gameAdd,global.givegame=givegame,global.cekGLimit=cekGLimit;
 const { color, getAdmin, isUrl } = require("./lib");
-const rzkyClient = require("rzkyfdlh-api");
-global.rzky = new rzkyClient("237991cy34fq2ct245fr2ojoqoset92ooua71r49i121x6b21k");
-global.response = require("./lib/response.json");
 const cooldown = new Map();
 const prefix = "#";
 const multi_pref = new RegExp("^[" + "!#%&?/;:,.~-+=".replace(/[|\\{}()[\]^$+*?.\-\^]/g, "\\$&") + "]");
-global.config = require("./config.json");
 const owner = config.owner;
 function printSpam(isGc, sender, groupName) {
 	if (isGc) {
