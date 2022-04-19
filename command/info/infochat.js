d7e84d113c8f30b313bb44be75b167b248784173
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-19 18:46:18 +0700

    Update infochat.js

--- command/info/infochat.js
+++ command/info/infochat.js
@@ -3,20 +3,20 @@ let baileys = require("@adiwajshing/baileys");
 module.exports = {
 	name: "infochat",
 	alias: ["sider"],
-	desc: "Melihat Siapa saya yang read pesan",
+	desc: "See Who I am who reads messages",
 	category: "info",
 	isQuoted: true,
 	async run(msg, conn) {
 		let message = await msg.getQuotedObj();
-		if (!msg.quoted.isSelf) throw "Pesan tersebut bukan dikirim oleh bot!";
+		if (!msg.quoted.isSelf) throw "The message was not sent by a bot!";
 		let teks = "";
 		for (let i of message.userReceipt) {
 			let read = i.readTimestamp;
 			let unread = i.receiptTimestamp;
 			let waktu = read ? read : unread;
 			teks += `⭔ @${i.userJid.split("@")[0]}\n`;
-			teks += ` ┗━⭔ *Waktu :* ${require("moment-timezone")(waktu * 1000).format("DD/MM/YY HH:mm:ss")}\n`;
-			teks += `⭔ *Status :* ${read ? "Dibaca" : "Terkirim"}\n\n`;
+			teks += ` ┗━⭔ *Time :* ${require("moment-timezone")(waktu * 1000).format("DD/MM/YY HH:mm:ss")}\n`;
+			teks += `⭔ *Status :* ${read ? "Be read" : "Sent"}\n\n`;
 		}
 		await msg.reply(teks, { withTag: true });
 	},
