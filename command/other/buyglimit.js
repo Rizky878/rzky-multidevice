c549779046f98fce9078790018b9bc5b45c91400
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-19 18:55:48 +0700

    update buyglimit.js

--- command/other/buyglimit.js
+++ command/other/buyglimit.js
@@ -3,19 +3,19 @@ module.exports = {
 	category: "other",
 	isSpam: true,
 	use: "<angka>",
-	query: "Kirim perintah *#buyglimit* jumlah limit yang ingin dibeli\n\nHarga 1 limit game = $150 balance",
+	query: "Send an order *#buyglimit* the limit amount you want to buy\n\nPrice for 1 game limit = $150 balance",
 	async run(msg, conn, q) {
-		if (q.includes("-")) throw `Jangan menggunakan -`;
-		if (isNaN(q)) throw `Harus berupa angka`;
+		if (q.includes("-")) throw `Don't use -`;
+		if (isNaN(q)) throw `Must be a number`;
 		let ane = Number(Math.floor(q) * 150);
-		if (getBalance(msg.sender, balance) < ane) throw `Balance kamu tidak mencukupi untuk pembelian ini`;
+		if (getBalance(msg.sender, balance) < ane) throw `Your balance is not sufficient for this purchase`;
 		kurangBalance(msg.sender, ane, balance);
 		givegame(msg.sender, Math.floor(q), glimit);
 		await msg.reply(
-			`Pembeliaan game limit sebanyak ${q} berhasil\n\nSisa Balance : $${getBalance(
+			`Purchase of game limit of ${q} was successful\n\nRemaining balance : $${getBalance(
 				msg.sender,
 				balance
-			)}\nSisa Game Limit : ${cekGLimit(msg.sender, gcount, glimit)}/${gcount}`
+			)}\nRemaining Game Limit : ${cekGLimit(msg.sender, gcount, glimit)}/${gcount}`
 		);
 	},
 };
