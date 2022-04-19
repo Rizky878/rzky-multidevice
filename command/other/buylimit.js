77dada7cd90b7153355c827cb09ec6c8650088d2
Rizky878 <74136457+Rizky878@users.noreply.github.com>
2022-04-19 18:58:24 +0700

    update buylimit.js

--- command/other/buylimit.js
+++ command/other/buylimit.js
@@ -3,19 +3,19 @@ module.exports = {
 	category: "other",
 	isSpam: true,
 	use: "<angka>",
-	query: "Kirim perintah *#buylimit* jumlah limit yang ingin dibeli\n\nHarga 1 limit = $150 balance",
+	query: "Send an order *#buylimit* the limit amount you want to buy\n\nPrice 1 limit = $150 balance",
 	async run(msg, conn, q) {
-		if (q.includes("-")) throw `Jangan menggunakan -`;
-		if (isNaN(q)) throw `Harus berupa angka`;
+		if (q.includes("-")) throw `Don't use -`;
+		if (isNaN(q)) throw `Must be a number`;
 		let ane = Number(Math.floor(q) * 150);
 		if (getBalance(msg.sender, balance) < ane) throw `Balance kamu tidak mencukupi untuk pembelian ini`;
 		kurangBalance(msg.sender, ane, balance);
 		giveLimit(msg.sender, Math.floor(q), limit);
 		await msg.reply(
-			`Pembeliaan limit sebanyak ${q} berhasil\n\nSisa Balance : $${getBalance(
+			`The limit purchase of ${q} was successful\n\nRemaining Balance : $${getBalance(
 				msg.sender,
 				balance
-			)}\nSisa Limit : ${getLimit(msg.sender, limitCount, limit)}/${limitCount}`
+			)}\nRemaining Limit : ${getLimit(msg.sender, limitCount, limit)}/${limitCount}`
 		);
 	},
 };
