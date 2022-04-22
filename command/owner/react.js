3e4f818a3dbec552759840fb94ade94c0104027a
Rizky878 <Rizky878@users.noreply.github.com>
2022-04-22 18:10:37 +0000

    automatic tidy up code

--- command/owner/react.js
+++ command/owner/react.js
@@ -1,19 +1,19 @@
 module.exports = {
-  name: "react",
-  alias: ["re"],
-  category: "private",
-  isOwner: true,
-  isSpam: true, 
-  desc: "Reaction message",
-  use: "<Tag Mess>",
-  query: "Masukkan emoji",
-  async run(msg, conn, q) {
-    const reactionMessage = {
-      react: {
-        text: `${q}`,
-        key: msg.quoted.key
-      }
-    }
-    await conn.sendMessage(msg.from, reactionMessage)
-  }
-}
+	name: "react",
+	alias: ["re"],
+	category: "private",
+	isOwner: true,
+	isSpam: true,
+	desc: "Reaction message",
+	use: "<Tag Mess>",
+	query: "Masukkan emoji",
+	async run(msg, conn, q) {
+		const reactionMessage = {
+			react: {
+				text: `${q}`,
+				key: msg.quoted.key,
+			},
+		};
+		await conn.sendMessage(msg.from, reactionMessage);
+	},
+};
