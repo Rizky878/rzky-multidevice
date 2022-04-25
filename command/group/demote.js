b847a58b9cdf9b3d47acd4c43eeff0a490ab25df
Rizky878 <Rizky878@users.noreply.github.com>
2022-04-25 14:18:31 +0000

    automatic tidy up code

--- command/group/demote.js
+++ command/group/demote.js
@@ -4,13 +4,14 @@ module.exports = {
 	category: "group",
 	desc: "Demote admin group",
 	use: "<tagMem>",
-	isGroup: true, 
+	isGroup: true,
 	isBotAdmin: true,
 	isAdmin: true,
 	async run(msg, conn) {
-		if (msg.message.extendedTextMessage === undefined || msg.message.extendedTextMessage === null) return msg.reply('Tag member!') 
-		const mm = msg.quoted.sender
-		for (let i of mm) await conn.groupParticipantsUpdate(msg.from, [i], "demote")
-		await msg.reply("Suksess")
-}
-}
+		if (msg.message.extendedTextMessage === undefined || msg.message.extendedTextMessage === null)
+			return msg.reply("Tag member!");
+		const mm = msg.quoted.sender;
+		for (let i of mm) await conn.groupParticipantsUpdate(msg.from, [i], "demote");
+		await msg.reply("Suksess");
+	},
+};
