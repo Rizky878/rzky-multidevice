20061c9c9b73110e93306d59594eff9755a17e7c
Zen <88880334+Znzn11@users.noreply.github.com>
2022-04-25 18:28:10 +0700

    Update demote.js

--- command/group/demote.js
+++ command/group/demote.js
@@ -6,7 +6,7 @@ module.exports = {
 	use: "<tagMem>",
 	isGroup: true, 
 	isBotAdmin: true,
-	isAdmin: true
+	isAdmin: true,
 	async run(msg, conn) {
 		if (msg.message.extendedTextMessage === undefined || msg.message.extendedTextMessage === null) return msg.reply('Tag member!') 
 		const mm = msg.quoted.sender
