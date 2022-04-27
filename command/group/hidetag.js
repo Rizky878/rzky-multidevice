dff87291162dcda35dd6184cccdf361d545fe61b
Rizky878 <Rizky878@users.noreply.github.com>
2022-04-27 20:58:41 +0000

    automatic tidy up code

--- command/group/hidetag.js
+++ command/group/hidetag.js
@@ -2,16 +2,16 @@ module.exports = {
 	name: "hidetag",
 	alias: ["ht"],
 	category: "group",
-	isAdmin: true, 
-	isBotAdmin: true, 
-	isGroup: true, 
+	isAdmin: true,
+	isBotAdmin: true,
+	isGroup: true,
 	desc: "Buat ngasih informasi / bikin 1 gc kesel :v",
 	use: "<text>",
 	query: "Masukkan teks",
 	async run(msg, conn, q) {
-		const gc = await conn.groupMetadata(msg.from)
-		const mem = gc.participants
-		
-		conn.sendMessage(msg.from, { text : `${q}`, mentions: mem.map(a => a.id)})
-}
-}
+		const gc = await conn.groupMetadata(msg.from);
+		const mem = gc.participants;
+
+		conn.sendMessage(msg.from, { text: `${q}`, mentions: mem.map((a) => a.id) });
+	},
+};
