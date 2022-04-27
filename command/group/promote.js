55324930672146ad32c1f7ebe758c9126332bef3
Rizky878 <Rizky878@users.noreply.github.com>
2022-04-27 23:02:12 +0000

    automatic tidy up code

--- command/group/promote.js
+++ command/group/promote.js
@@ -8,8 +8,7 @@ module.exports = {
 	isBotAdmin: true,
 	isAdmin: true,
 	async run(msg, conn) {
-		if (!msg.quoted && msg.mentions[0])
-			return msg.reply("Tag member!");
+		if (!msg.quoted && msg.mentions[0]) return msg.reply("Tag member!");
 		const mm = msg.quoted ? msg.mentions : [msg.quoted.sender];
 		for (let i of mm) await conn.groupParticipantsUpdate(msg.from, [i], "promote");
 		await msg.reply("Suksess");
