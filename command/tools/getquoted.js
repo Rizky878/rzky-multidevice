4eee9430e31afbe9d701e7a8991422a2ebc20881
Rizky878 <Rizky878@users.noreply.github.com>
2022-04-30 21:19:07 +0000

    automatic tidy up code

--- command/tools/getquoted.js
+++ command/tools/getquoted.js
@@ -4,9 +4,9 @@ module.exports = {
 	category: "tools",
 	isQuoted: true,
 	async run({ msg, conn }) {
-	  const { serialize } = require("../../lib/serialize")
-	  const message = await msg.getQuotedObj()
-	  if(!message.quoted) throw "The message you replied does not contain a reply"
-		conn.sendMessage(msg.from, { forward: await serialize(message.quoted, conn) })
+		const { serialize } = require("../../lib/serialize");
+		const message = await msg.getQuotedObj();
+		if (!message.quoted) throw "The message you replied does not contain a reply";
+		conn.sendMessage(msg.from, { forward: await serialize(message.quoted, conn) });
 	},
 };
