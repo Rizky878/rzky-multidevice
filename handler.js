c432d2dd4642d4d2d98b4c5c47d21bafb4fde306
Rizky Fadilah <74136457+Rizky878@users.noreply.github.com>
2022-05-11 22:18:22 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -77,7 +77,7 @@ module.exports = handler = async (m, conn, map) => {
 		const arg = body.substring(body.indexOf(" ") + 1);
 		const args = body.trim().split(/ +/).slice(1);
 		const comand = body.trim().split(/ +/)[0];
-		let q = arg;
+		let q = body.trim().split(/ +/).slice(1).join(" ");
 		const isCmd = body.startsWith(temp_pref);
 
 		//type message
