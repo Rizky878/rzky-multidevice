87fbda11daa40033dc3bc9ca8cd7f9ac876bec36
Rizky878 <Rizky878@users.noreply.github.com>
2022-05-12 05:21:57 +0000

    automatic tidy up code

--- command/group/add.js
+++ command/group/add.js
@@ -9,7 +9,7 @@ module.exports = {
 	isAdmin: true,
 	isSpam: true,
 	async run({ msg, conn }, { q, prefix }) {
-	  add = q ? q : msg.quoted ? msg.quoted : false
+		add = q ? q : msg.quoted ? msg.quoted : false;
 		if (!add) return msg.reply("Example: " + prefix + "add 62728288");
 		q = msg.quoted ? msg.quoted.sender.split("@")[0] : q;
 		let prk = q.replace(/[^a-zA-Z0-9 ]/g, "").split(" ");
