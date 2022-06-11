a13a85964c9c4deb05af314e06bb3080a15dee52
Rizky Fadilah <74136457+Rizky878@users.noreply.github.com>
2022-06-11 22:39:36 +0700

    Update global.js

--- global.js
+++ global.js
@@ -7,6 +7,8 @@ global.reloadFile = (file, options = {}) => {
 }
 const{isLimit:isLimit,limitAdd:limitAdd,getLimit:getLimit,giveLimit:giveLimit,addBalance:addBalance,kurangBalance:kurangBalance,getBalance:getBalance,isGame:isGame,gameAdd:gameAdd,givegame:givegame,cekGLimit:cekGLimit}=require("./lib/limit");global.prem=require("./lib/premium"),global.limit=JSON.parse(fs.readFileSync("./database/limit.json")),global.glimit=JSON.parse(fs.readFileSync("./database/glimit.json")),global.balance=JSON.parse(fs.readFileSync("./database/balance.json")),global.premium=JSON.parse(fs.readFileSync("./database/premium.json")),global.isLimit=isLimit,global.limitAdd=limitAdd,global.getLimit=getLimit,global.giveLimit=giveLimit,global.addBalance=addBalance,global.kurangBalance=kurangBalance,global.getBalance=getBalance,global.isGame=isGame,global.gameAdd=gameAdd,global.givegame=givegame,global.cekGLimit=cekGLimit;
 const IkyyClient = require("ikyy");
+const { ikyEvent } = require("./lib/Event")
+global.ikyEvent = ikyEvent
 global.rzky = new IkyyClient();
 global.response = require("./lib/response.json");
 global.config = require("./config.json");
