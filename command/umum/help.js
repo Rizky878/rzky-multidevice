ab1b87eb36956712c9294c409979b277250ca7ef
Rizky Fadilah <74136457+Rizky878@users.noreply.github.com>
2022-06-11 22:36:27 +0700

    Update help.js

--- command/umum/help.js
+++ command/umum/help.js
@@ -1,15 +1,21 @@
+const hari = moment.tz(config.timezone).format("a");
+const ucapanWaktu = hari.charAt(0).toUpperCase() + hari.slice(1);
+const processTime = (timestamp, now) => {
+	return moment.duration(now - moment(timestamp * 1000)).asSeconds();
+};
+
 module.exports = {
 	name: "help",
 	alias: ["h", "cmd", "menu"],
 	category: "umum",
 	isLimit: true,
-	async run({ msg, conn }, { q, map, args }) {
+	async run({ msg, conn }, { q, owner, map, args }) {
 		if (q) {
 			const data = [];
 			const name = q.toLowerCase();
 			const { command, prefix } = map;
 			const cmd = command.get(name) || [...command.values()].find((x) => x.alias.find((x) => x == args[0]));
-			if (!cmd || (cmd.category === "private" && !config.owner.includes(msg.sender)))
+			if (!cmd || (cmd.category === "hidden" && !config.owner.includes(msg.sender)))
 				return await msg.reply("Command not found");
 			else data.push(`*Name:* ` + cmd.name);
 			if (cmd.alias) data.push(`*Alias:* ${cmd.alias.join(", ")}`);
@@ -23,6 +29,10 @@ module.exports = {
 			const { prefix, command } = map;
 			const cmds = command.keys();
 			let category = [];
+			const xes = require("parse-ms")(prem.getPremiumExpired(msg.sender, premium) - Date.now());
+			dashboard = dashboard.sort(function (a, b) {
+				return b.success - a.success;
+			});
 
 			for (let cmd of cmds) {
 				let info = command.get(cmd);
@@ -30,6 +40,7 @@ module.exports = {
 				if (config.ignore.directory.includes(info.category.toLowerCase())) continue;
 				cteg = info.category || "No Category";
 				if (info.type == "changelog") continue;
+				if (cteg == "hidden") continue;
 				if (!cteg || cteg === "private") cteg = "owner command";
 				if (Object.keys(category).includes(cteg)) category[cteg].push(info);
 				else {
@@ -37,41 +48,53 @@ module.exports = {
 					category[cteg].push(info);
 				}
 			}
-			let str =
-				"```" +
-				config.namebot +
-				"```\n\n" +
-				`Halo, ${pushName === undefined ? sender.split("@")[0] : pushName}\n*Here is the Command List*\n\n`;
+			let str = `「 *${config.namebot}* 」
+	
+◪ *Time*
+❏ ${moment.tz(config.timezone).format("HH:mm:ss")}
+
+◪ *Speed*
+❏ ${processTime(msg.messageTimestamp, moment())} _seconds_
+
+◪ *Date*
+❏ ${moment.tz(config.timezone).format("dddd, DD/MM/YYYY")}
+
+◪ *INFO USER*
+❏ Nomer: 「  ${msg.sender.split("@")[0]} 」
+❏ Nama: 「  ${conn.getName(msg.sender)} 」
+❏ Status: 「 ${isPremium ? "Premium" : owner ? "Owner" : "Standar"} 」
+${isPremium ? `❏ Expired: 「 ${xes.days} D ${xes.hours} H ${xes.minutes} M 」\n` : ""}
+
+◪ *Fitur terpopuler saat ini*
+1. *${prefix}${dashboard[0].name}* dipakai sebanyak *${dashboard[0].success + dashboard[0].failed}* kali
+2. *${prefix}${dashboard[1].name}* dipakai sebanyak *${dashboard[1].success + dashboard[1].failed}* kali
+3. *${prefix}${dashboard[2].name}* dipakai sebanyak *${dashboard[2].success + dashboard[2].failed}* kali\n\n`;
 			const keys = Object.keys(category);
 			//var a = 1
 			for (const key of keys) {
-				str += `==== [ *${key.toUpperCase()}* ] ====\n${category[key]
+				str += `*❏ ${key.toUpperCase()}*\n${category[key]
 					.map(
 						(cmd, index) =>
-							`*${index + 1}.* *${cmd.options.noPrefix ? "" : "#"}${cmd.name}*${
-								cmd.alias[0]
-									? "\n" +
-									  cmd.alias
-											.map((a) => (a ? `*▸ ${cmd.options.noPrefix ? "" : "#"}${a}*` : ""))
-											.join("\n")
+							`*${index + 1}.* *${cmd.options.noPrefix ? "" : "#"}${cmd.name}* ${
+								cmd.category == "private"
+									? ""
+									: cmd.use
+									? cmd.use.replace(">", " 」").replace("<", "「 ")
 									: ""
-							}\n*Usage:* _${cmd.options.noPrefix ? "" : "#"}${cmd.name}${
-								cmd.category == "private" ? "" : cmd.use ? " " + cmd.use : ""
-							}_`
+							}`
 					)
-					.join("\n\n")}\n\n`;
+					.join("\n")}\n\n`;
 			}
 			str += `typing *${prefix}help sticker* for get the details and example use`;
 			await conn.sendMessage(
 				msg.from,
 				{
-					video: await conn.getBuffer(config.thumbvideo),
-					gifPlayback: true,
+					location: { jpegThumbnail: await conn.getBuffer(config.thumb) },
 					caption: str,
 					footer: config.namebot + " • " + config.ownername,
 					templateButtons: [
 						{ urlButton: { displayText: "Shortlink", url: "https://sl.rzkyfdlh.tech" } },
-						{ urlButton: { displayText: "Downloader", url: "https://downloader.rzkyfdlh.tech" } },
+						{ urlButton: { displayText: "Downloader", url: "https://down.rzkyfdlh.tech" } },
 						{ quickReplyButton: { displayText: "Script Bot📑", id: "#script" } },
 						{ quickReplyButton: { displayText: "Changelog📋", id: "#changelog" } },
 						{ quickReplyButton: { displayText: "Dashboard📊", id: "#db" } },
