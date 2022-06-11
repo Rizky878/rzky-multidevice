f212dd544cde2d44547564672b2aef2ebb5a1b6b
Rizky Fadilah <74136457+Rizky878@users.noreply.github.com>
2022-06-11 22:37:14 +0700

    Update tebakbendera.js

--- command/game/tebakbendera.js
+++ command/game/tebakbendera.js
@@ -6,16 +6,18 @@ module.exports = {
 	category: "game",
 	desc: "Play games, guess the country flag",
 	isSpam: true,
+	isGroup: true,
 	isLimitGame: true,
 	async run({ msg, conn }, { q, map }) {
 		if (cekStatus(msg.from, map, "tebakbendera")) throw "Group Ini masih dalam sesi permainan";
 		let waktugame = 60;
 		let tebakbendera = await rzky.game.tebakbendera();
 		if (tebakbendera) {
-			addSesi(msg.from, tebakbendera.jawaban, waktugame, map, "tebakbendera");
-			await msg.reply(
-				`*Game Tebak Bendera*\n\nBendera: ${tebakbendera.bendera}\nHint: ${tebakbendera.pertanyaan}\n\nAnswered Immediately, Time only 1 minute!`
+			const { key } = await msg.reply(
+				`*Game Tebak Bendera*\n\nBendera: ${tebakbendera.bendera}\nHint: ${tebakbendera.pertanyaan}\n\nAnswered Immediately, Time only 1 minute!\n\n*Happy Answering!*`
 			);
+			addSesi(msg.from, key.id, tebakbendera.jawaban, waktugame, map, "tebakbendera");
+			conn.game[key.id] = { status: false };
 		} else msg.reply("Error");
 	},
 };
