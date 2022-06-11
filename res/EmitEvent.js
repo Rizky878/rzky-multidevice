ac470b913875e7bc2eb08d8d6120381fc18a48a2
Rizky Fadilah <74136457+Rizky878@users.noreply.github.com>
2022-06-11 22:28:38 +0700

    Create EmitEvent.js

---
+++ res/EmitEvent.js
@@ -0,0 +1,15 @@
+
+module.exports = async (msg, conn) => {
+	const { from, isGroup, msgType, sender, key, body } = msg;
+
+	if (msgType == "viewOnceMessage") {
+		messagee = { ...msg };
+		messagee.message[Object.keys(msg.message)[0]].viewOnce = false;
+		const opt = {
+			remoteJid: key.remoteJid,
+			participant: sender,
+			message: messagee,
+		};
+		ikyEvent.emit("viewOnceMessage", opt);
+}
+};
