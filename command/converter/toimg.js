a59698b26ec0466a8fd21ea61a9992861a540a99
Ma'ruf Ma'arij <72728486+mrfzvx12@users.noreply.github.com>
2022-07-09 17:38:35 +0700

    Add files via upload

---
+++ command/converter/toimg.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports = {
+	name: "Toimg",
+	alias: ["toimg"],
+	category: "converter",
+	desc: "Convert a sticker to image",
+	
+	async run({ msg, conn }, { q }) {
+		const { quoted, from, type } = msg;
+		const content = JSON.stringify(quoted); 
+		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage"); 
+		let buffer, stickerBuff;
+		try {
+			if (isQStic) {
+				buffer = await quoted.download();
+				await conn.sendFile(msg.from, buffer, "toimg.jpg", 'Success', msg);
+			} else if {
+			  await msg.reply(`Reply sticker`);
+			}
+		 	 (buffer = null), (stickerBuff = null);
+		} catch (e) {
+			console.log(e);
+			await msg.reply("Error convert sticker");
+		}
+	}
+}
---
+++ command/converter/tomp4.js
@@ -0,0 +1,27 @@
+let { webp2mp4 } = require('../../lib/webp2')
+
+module.exports = {
+	name: "togif",
+	alias: ["togif", "tomp4"],
+	category: "converter",
+	desc: "Convert a sticker to gif",
+	wait: true,
+	async run({ msg, conn }, { q }) {
+		const { quoted, from, type } = msg;
+		const content = JSON.stringify(quoted); 
+		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage"); 
+	
+	try {
+	  if (isQStic) {
+        let media = await quoted.download()
+        out = await webp2mp4(media)
+      await conn.sendFile(msg.from, out, "video.mp4", 'Success', msg);
+	  } else {
+	    await msg.reply(`Reply sticker`);
+	  }
+	  } catch (e) {
+	    console.log(e);
+			await msg.reply("Error while convert sticker");
+	}
+	},
+};
