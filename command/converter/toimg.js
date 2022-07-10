00ad47b6e7ba2afc01ae0ad16c47cae09c8fc4d6
Ma'ruf Ma'arij <72728486+mrfzvx12@users.noreply.github.com>
2022-07-10 15:07:08 +0700

    typo

--- command/converter/toimg.js
+++ command/converter/toimg.js
@@ -1,24 +1,27 @@
-const fs = require("fs");
+let { webp2mp4 } = require('../../lib/webp2')
+
 module.exports = {
 	name: "toimg",
+	alias: ["toimg"],
 	category: "converter",
 	desc: "Convert a sticker to image",
+	wait: true,
 	async run({ msg, conn }, { q }) {
 		const { quoted, from, type } = msg;
-		const content = JSON.stringify(quoted);
-		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage");
-		let buffer, stickerBuff;
-		try {
-			if (isQStic) {
-				buffer = await quoted.download();
-				await conn.sendFile(msg.from, buffer, "toimg.jpg", "Success", msg);
-			} else {
-				await msg.reply(`Reply sticker`);
-			}
-			(buffer = null), (stickerBuff = null);
-		} catch (e) {
-			console.log(e);
-			await msg.reply("Error convert sticker");
-		}
+		const content = JSON.stringify(quoted); 
+		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage"); 
+	
+	try {
+	  if (isQStic) { 
+        let media = await quoted.download()
+        out = await webp2mp4(media)
+        await conn.sendFile(msg.from, out, "image.mp4", 'Success', msg);
+	  } else {
+	    await msg.reply(`Reply sticker`);
+	  }
+	  } catch (e) {
+	    console.log(e);
+			await msg.reply("Error while convert sticker");
+	}
 	},
 };
