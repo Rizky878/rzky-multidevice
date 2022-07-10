9770d520da63a254c3a75a7caea4dbaf17d9a990
Rizky878 <Rizky878@users.noreply.github.com>
2022-07-10 04:27:10 +0000

    automatic tidy up code

--- command/converter/toimg.js
+++ command/converter/toimg.js
@@ -6,20 +6,20 @@ module.exports = {
 	desc: "Convert a sticker to image",
 	async run({ msg, conn }, { q }) {
 		const { quoted, from, type } = msg;
-		const content = JSON.stringify(quoted); 
-		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage"); 
+		const content = JSON.stringify(quoted);
+		const isQStic = type === "extendedTextMessage" && content.includes("stickerMessage");
 		let buffer, stickerBuff;
 		try {
 			if (isQStic) {
 				buffer = await quoted.download();
-				await conn.sendFile(msg.from, buffer, "toimg.jpg", 'Success', msg);
+				await conn.sendFile(msg.from, buffer, "toimg.jpg", "Success", msg);
 			} else {
-			  await msg.reply(`Reply sticker`);
+				await msg.reply(`Reply sticker`);
 			}
-		 	 (buffer = null), (stickerBuff = null);
+			(buffer = null), (stickerBuff = null);
 		} catch (e) {
 			console.log(e);
 			await msg.reply("Error convert sticker");
 		}
-	}
-}
+	},
+};
