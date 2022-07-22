8d6cdafef35b2d305f9a722bea9fff4e1029f274
Rizky878 <Rizky878@users.noreply.github.com>
2022-07-22 05:15:03 +0000

    automatic tidy up code

--- command/info/owner.js
+++ command/info/owner.js
@@ -5,6 +5,6 @@ module.exports = {
 	isSpam: true,
 	async run({ msg, conn }, { q, map, args }) {
 		var msga = await conn.sendContact(msg.from, config.owner, msg);
-		await conn.sendMessage(msg.from, { text: `this my owner number, please don't spam` }, { });
+		await conn.sendMessage(msg.from, { text: `this my owner number, please don't spam` }, {});
 	},
 };
