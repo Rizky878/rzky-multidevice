f8177123491b8d06bf00f4cef97f62397906b871
ᴢɪxʏ.ᴇᴀꜱᴘᴏʀᴛ.xxx <107806210+Ikymd@users.noreply.github.com>
2022-07-31 15:13:34 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const { color, getAdmin, isUrl } = require("./lib");
 const cooldown = new Map();
 const prefix = "#";
-const multi_pref = new RegExp("^[" + "!#%&?/;:,.~-+=".replace(/[|\\{}()[\]^$+*?.\-\^]/g, "\\$&") + "]");
+const multi_pref = new RegExp("^[" + "!#%&?/;:,~-+=".replace(/[|\\{}()[\]^$+*?.\-\^]/g, "\\$&") + "]");
 const owner = config.owner;
 function printSpam(conn, isGc, sender, groupName) {
 	if (isGc) {
